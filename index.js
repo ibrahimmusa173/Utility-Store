6c7234fbf719a46a284fa2e33937b304ffcb69ca
ibrahimmusa173 <musaaman705@gmail.com>
2025-07-07 00:17:44 -0700

    ok

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const RouterPath = require("./router");
+
+// const app = express();
+// const port = 7000;
+
+// app.use(bodyParser.json());
+// app.use(cors({
+//     origin: "http://localhost:3000", // Allow requests from frontend
+//     methods: ["GET", "POST",'PUT', 'DELETE'], // Allow specific methods
+//     credentials: true // If you need to send cookies or auth headers
+// }));
+
+// app.use("/", RouterPath); // Mount router at root
+
+// app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const RouterPath = require("./router");
 
-const app = express();
-const port = 7000;
+const app= express();
+
+const port= 7000;
 
 app.use(bodyParser.json());
-app.use(cors({
-    origin: "http://localhost:3000", // Allow requests from frontend
-    methods: ["GET", "POST",'PUT', 'DELETE'], // Allow specific methods
-    credentials: true // If you need to send cookies or auth headers
-}));
-
-app.use("/", RouterPath); // Mount router at root
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.use(cors());
+app.listen(port, ()=>console.log("server running"));
